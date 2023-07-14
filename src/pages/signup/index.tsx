87ec93b2430ecb4c75970e0ab5e37b1ee41a9fdf
Pyotato <pyolovely01@gmail.com>
2023-07-15 02:14:22 +0900

    signup page issue fix

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,9 +1,8 @@
 import SignupForm from '../../components/SignupForm';
 // import { useNavigate } from 'react-router-dom';
 function SignupPage() {
-//   const navigate = useNavigate();
-    return <SignupForm />;
-
+  //   const navigate = useNavigate();
+  return <SignupForm />;
 }
 
 export default SignupPage;
