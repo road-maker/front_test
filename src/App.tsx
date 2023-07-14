cb13d1e44a0d4260bea0c5e8bc127f8601015ca1
Pyotato <pyolovely01@gmail.com>
2023-07-14 20:09:23 +0900

    test s3 upload , wrap with tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world!</div>;
+  return (
+    <div className="App">
+      <div>hello world!</div>
+    </div>
+  );
 }
 
 export default App;
