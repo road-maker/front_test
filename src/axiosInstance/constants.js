b5714bf3d2037c7bc5fcad7511f4d0cdda97ff8e
Pyotato <pyolovely01@gmail.com>
2023-07-15 04:45:02 +0900

    attempting to connect with spring

--- src/axiosInstance/constants.js
+++ src/axiosInstance/constants.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://localhost:8080';
+export const baseUrl = 'http://3.35.206.244:8080';
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,14 @@
+import { axiosInstance } from 'axiosInstance';
 import { Link, useNavigate } from 'react-router-dom';
 
 function MainPage() {
   const navigate = useNavigate();
+
+  async function sayHello(): Promise<string> {
+    const { data } = await axiosInstance.get('/hello');
+    return data;
+  }
+
   return (
     <>
       <h1>MainPage</h1>
@@ -9,6 +16,7 @@ function MainPage() {
       <button type="button" onClick={() => navigate('/roadmap/editor')}>
         editor page
       </button>
+      <div>{sayHello()}</div>
     </>
   );
 }
