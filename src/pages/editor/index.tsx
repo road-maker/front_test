12764c785297bb36f612b11935a405b534ee1542
Pyotato <pyolovely01@gmail.com>
2023-07-16 16:25:53 +0900

    [#14] Feat: React Flow 라이브러리 적용 테스트, autoformating 적용

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -2,16 +2,6 @@ import Editor from 'components/editor/Editor';
 import { ReactElement } from 'react';
 
 function EditorPage(): ReactElement {
-  // const canvasRef = useRef<HTMLCanvasElement>(null);
-  // const canvas = canvasRef.current;
-
-  // if (!canvas) return <div>null canvas!</div>;
-
-  return (
-    <div>
-      {/* <canvas ref={canvasRef} /> */}
-      <Editor />
-    </div>
-  );
+  return <Editor />;
 }
 export default EditorPage;
