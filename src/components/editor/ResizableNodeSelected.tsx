4f1e6343319813c9115d2a2a29664672086b223c
Pyotato <pyolovely01@gmail.com>
2023-07-19 00:36:18 +0900

    [#24] Fix :  resizeobserver-loop-limit-exceeded-error 에러 처리

--- src/components/editor/ResizableNodeSelected.tsx
+++ src/components/editor/ResizableNodeSelected.tsx
@@ -1,8 +1,26 @@
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import { Handle, NodeResizer, Position } from 'reactflow';
 
 function ResizableNodeSelected({ data, selected }) {
   // window.ResizeObserver = undefined;
+  useEffect(() => {
+    window.addEventListener('error', (e) => {
+      if (e.message === 'ResizeObserver loop limit exceeded') {
+        const resizeObserverErrDiv = document.getElementById(
+          'webpack-dev-server-client-overlay-div',
+        );
+        const resizeObserverErr = document.getElementById(
+          'webpack-dev-server-client-overlay',
+        );
+        if (resizeObserverErr) {
+          resizeObserverErr.setAttribute('style', 'display: none');
+        }
+        if (resizeObserverErrDiv) {
+          resizeObserverErrDiv.setAttribute('style', 'display: none');
+        }
+      }
+    });
+  }, []);
   return (
     <>
       <NodeResizer
