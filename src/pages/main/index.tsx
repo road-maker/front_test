eed2ca6df7eadfa01a909a4d55872be0676a3442
Seoin Jeon <sower031@gmail.com>
2023-07-18 22:00:37 +0900

    [#7]Feat: 메인화면 임시구현 완료

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,76 +1,8 @@
-// import {
-//   ActionIcon,
-//   AppShell,
-//   Avatar,
-//   Header,
-//   MantineProvider,
-//   Navbar,
-//   Text,
-//   TextInput,
-//   TextInputProps,
-//   useMantineTheme,
-// } from '@mantine/core';
-// import {
-//   IconAlertCircle,
-//   IconArrowLeft,
-//   IconArrowRight,
-//   IconSearch,
-// } from '@tabler/icons-react';
-
-// function MainPage() {
-//   return (
-//     <AppShell
-//       display="flex"
-//       p="md"
-//       header={
-//         <Header height={60} p="md" display="flex">
-//           <h2>RoadMaker</h2>
-//           <InputWithButton />
-//           <IconAlertCircle size="2rem" />
-//           <Avatar src={null} alt="no image here" />
-//         </Header>
-//       }
-//       styles={(theme) => ({
-//         main: {
-//           backgroundColor:
-//             theme.colorScheme === 'dark'
-//               ? theme.colors.dark[8]
-//               : theme.colors.gray[0],
-//         },
-//       })}
-//     >
-//       {/* Your application here */}
-//     </AppShell>
-//   );
-// }
-
-// export default MainPage;
-
-// export function Demo() {
-//   return (
-//     <MantineProvider
-//       theme={{
-//         headings: {
-//           // properties for all headings
-//           fontFamily: 'Roboto',
-//           // properties for individual headings, all of them are optional
-//           sizes: {
-//             h1: { fontSize: '3rem', lineHeight: 1.4 },
-//             h2: { fontSize: '2rem' },
-//             // ...up to h6
-//             h6: { fontWeight: 900 },
-//           },
-//         },
-//       }}
-//     >
-//       <MainPage />
-//     </MantineProvider>
-//   );
-// }
 import { Button } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
 
 import { HeaderMegaMenu } from './header';
+import { RoadmapRecommendation } from './roadmapRecommendation';
 
 function MainPage() {
   const navigate = useNavigate();
@@ -78,6 +10,7 @@ function MainPage() {
     <>
       <HeaderMegaMenu />
       <Button onClick={() => navigate('roadmap/editor')}>Editor Page</Button>
+      <RoadmapRecommendation />
     </>
   );
 }
---
+++ src/pages/main/roadmapRecommendation.tsx
@@ -0,0 +1,136 @@
+import {
+  ActionIcon,
+  Avatar,
+  Card,
+  Container,
+  createStyles,
+  Group,
+  Image,
+  rem,
+  SimpleGrid,
+  Text,
+} from '@mantine/core';
+import { IconBookmark, IconHeart, IconShare } from '@tabler/icons-react';
+
+const mockdata = [
+  {
+    id: 1,
+    title: 'Javascript 정복하기',
+    category: 'Frontend',
+    image: 'https://t1.daumcdn.net/cfile/tistory/21221F4258E793521D',
+    date: 'August 18, 2022',
+    author: {
+      name: '표혜민',
+      description: '한 번 정복해보아요~^^',
+    },
+  },
+  {
+    id: 2,
+    title: 'Javascript 정복하기',
+    category: 'Frontend',
+    image: 'https://t1.daumcdn.net/cfile/tistory/21221F4258E793521D',
+    date: 'August 18, 2022',
+    author: {
+      name: '표혜민',
+      description: '한 번 정복해보아요~^^',
+    },
+  },
+  {
+    id: 3,
+    title: 'Javascript 정복하기',
+    category: 'Frontend',
+    image: 'https://t1.daumcdn.net/cfile/tistory/21221F4258E793521D',
+    date: 'August 18, 2022',
+    author: {
+      name: '표혜민',
+      description: '한 번 정복해보아요~^^',
+      profile: 'avatar.png',
+    },
+  },
+];
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+  },
+
+  author: {
+    padding: `${theme.spacing.xs} ${theme.spacing.lg}`,
+    marginTop: theme.spacing.md,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+    fontSize: '13px',
+  },
+}));
+
+export function RoadmapRecommendation() {
+  const { classes, theme } = useStyles();
+
+  const cards = mockdata.map((article) => (
+    <Card
+      key={article.id}
+      p="xs"
+      radius="md"
+      component="a"
+      className={classes.card}
+    >
+      <Card.Section mb="sm">
+        <Image
+          src={article.image}
+          alt={article.title}
+          height={200}
+          width={280}
+        />
+      </Card.Section>
+      <Text color="dimmed" size="xs" transform="uppercase" weight={700} mt="md">
+        {article.date}
+      </Text>
+      <Text className={classes.title} mt={5}>
+        {article.title}
+      </Text>
+      <Text fz="sm" color="dimmed" lineClamp={4} mt={5}>
+        {article.author.description}
+      </Text>
+      <Group spacing={5}>
+        <Avatar src={article.author.profile} alt="it's me" />
+        <Text className={classes.author} mt={5}>
+          {article.author.name}
+        </Text>
+        <ActionIcon>
+          <IconHeart size="1.2rem" color={theme.colors.red[6]} stroke={1.5} />
+        </ActionIcon>
+        <ActionIcon>
+          <IconBookmark
+            size="1.2rem"
+            color={theme.colors.yellow[6]}
+            stroke={1.5}
+          />
+        </ActionIcon>
+        <ActionIcon>
+          <IconShare size="1.2rem" color={theme.colors.blue[6]} stroke={1.5} />
+        </ActionIcon>
+      </Group>
+    </Card>
+  ));
+
+  return (
+    <Container py="sm">
+      <h1>추천 로드맵</h1>
+      <SimpleGrid
+        cols={3}
+        breakpoints={[
+          { maxWidth: 'sm', cols: 2 },
+          { maxWidth: 'sm', cols: 1 },
+        ]}
+      >
+        {cards}
+      </SimpleGrid>
+    </Container>
+  );
+}
