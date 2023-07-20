1c4bb3ab575a63ed0ab85fe382c31e73278cf190
Pyotato <pyolovely01@gmail.com>
2023-07-20 09:46:36 +0900

    [#25] Fix : 빌드 에러 해결 (remove unused imports)

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,6 +1,3 @@
-import { Button } from '@mantine/core';
-import { useNavigate } from 'react-router-dom';
-
 import { HeaderMegaMenu } from './header';
 import { RoadmapRecommendation } from './roadmapRecommendation';
 
